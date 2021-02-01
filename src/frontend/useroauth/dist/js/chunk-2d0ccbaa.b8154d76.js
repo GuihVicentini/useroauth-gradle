6732f63e23bdeb4d633d375441223c8160157941
guihvicentini <guilherme.h.vicentini@hotmail.com>
2021-02-01 21:58:47 +0100

    frontend group form

---
+++ src/frontend/useroauth/dist/js/chunk-2d0ccbaa.b8154d76.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0ccbaa"],{"4ebe":function(e,t,n){"use strict";n.r(t);var u=function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div")},r=[],s={name:"Group",computed:{currentUser:function(){return this.$store.state.auth.user}},mounted:function(){this.currentUser||this.$router.push("/login")}},c=s,o=n("2877"),i=Object(o["a"])(c,u,r,!1,null,null,null);t["default"]=i.exports}}]);
+//# sourceMappingURL=chunk-2d0ccbaa.b8154d76.js.map
